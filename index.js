37916fe8e2369a28fe87c6d9c7e08852588eb125
PeterSpace <98526111+peterspace@users.noreply.github.com>
2024-06-25 17:34:08 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ const app_access_token = process.env.FACEBOOK_ACCESS_TOKEN;
 //Step1: initial path
 
 // const link1 =
-//   "https://www.plinsters.pro/?sub1=NPR&sub2=291735090&fbp=714981180129689&token=EAAEcIRgo4MIBO7Gb3oGV6rbcjXOiZBhplvcAeWAXc6Xfn0xZAv02XEts1RyAcV7zEbY6mbYBqPgjUKY6PWhRrRf0YWHkzBToto5Q6rSJ4RqDWg8u84mKzhC28AeZBv1EXYGfCj1NZBTNPTH7ejqdUtCZA7ZCIgvZAZBuGqEpySTJOCgz6aIQawJfcsQBRGiuTiPh7AZDZD";
+//   "https://www.dmtgames.pro/?sub1=NPR&sub2=291735090&fbp=714981180129689&token=EAAEcIRgo4MIBO7Gb3oGV6rbcjXOiZBhplvcAeWAXc6Xfn0xZAv02XEts1RyAcV7zEbY6mbYBqPgjUKY6PWhRrRf0YWHkzBToto5Q6rSJ4RqDWg8u84mKzhC28AeZBv1EXYGfCj1NZBTNPTH7ejqdUtCZA7ZCIgvZAZBuGqEpySTJOCgz6aIQawJfcsQBRGiuTiPh7AZDZD";
 // const link2 =
-//   "https://www.plinsters.pro/?sub1=NPR&fbp=714981180129689&token=EAAEcIRgo4MIBO7Gb3oGV6rbcjXOiZBhplvcAeWAXc6Xfn0xZAv02XEts1RyAcV7zEbY6mbYBqPgjUKY6PWhRrRf0YWHkzBToto5Q6rSJ4RqDWg8u84mKzhC28AeZBv1EXYGfCj1NZBTNPTH7ejqdUtCZA7ZCIgvZAZBuGqEpySTJOCgz6aIQawJfcsQBRGiuTiPh7AZDZD";
+//   "https://www.dmtgames.pro/?sub1=NPR&fbp=714981180129689&token=EAAEcIRgo4MIBO7Gb3oGV6rbcjXOiZBhplvcAeWAXc6Xfn0xZAv02XEts1RyAcV7zEbY6mbYBqPgjUKY6PWhRrRf0YWHkzBToto5Q6rSJ4RqDWg8u84mKzhC28AeZBv1EXYGfCj1NZBTNPTH7ejqdUtCZA7ZCIgvZAZBuGqEpySTJOCgz6aIQawJfcsQBRGiuTiPh7AZDZD";
 // const link3 =
-//   "https://www.plinsters.pro/?sub1=NPR&sub2=291735090&sub3=NPR&sub4=vidos1&sub5={{ad.id}}&sub6=method1&fbp=714981180129689&token=EAAEcIRgo4MIBO7Gb3oGV6rbcjXOiZBhplvcAeWAXc6Xfn0xZAv02XEts1RyAcV7zEbY6mbYBqPgjUKY6PWhRrRf0YWHkzBToto5Q6rSJ4RqDWg8u84mKzhC28AeZBv1EXYGfCj1NZBTNPTH7ejqdUtCZA7ZCIgvZAZBuGqEpySTJOCgz6aIQawJfcsQBRGiuTiPh7AZDZD";
+//   "https://www.dmtgames.pro/?sub1=NPR&sub2=291735090&sub3=NPR&sub4=vidos1&sub5={{ad.id}}&sub6=method1&fbp=714981180129689&token=EAAEcIRgo4MIBO7Gb3oGV6rbcjXOiZBhplvcAeWAXc6Xfn0xZAv02XEts1RyAcV7zEbY6mbYBqPgjUKY6PWhRrRf0YWHkzBToto5Q6rSJ4RqDWg8u84mKzhC28AeZBv1EXYGfCj1NZBTNPTH7ejqdUtCZA7ZCIgvZAZBuGqEpySTJOCgz6aIQawJfcsQBRGiuTiPh7AZDZD";
 
 // add advertiser_tracking_id to installed API call in unity app
 app.get("/", async (req, res) => {
@@ -89,6 +89,30 @@ app.get("/", async (req, res) => {
     if (newUser) {
       facebookLink = updatedLink;
       console.log({ "New user created": newUser });
+      const appStoreLink = process.env.APP_STORE_LINK;
+      console.log("app install in progress");
+      return res.redirect(appStoreLink);
+    }
+  }
+
+  if (
+    advertiser_tracking_id &&
+    userTrackingIdExists &&
+    advertiser_tracking_id != userExists?.advertiserTrackingId
+  ) {
+    console.log("new user");
+
+    const newUser = await User.create({
+      ipAddress: ip,
+      userLink: updatedLink,
+      advertiserTrackingId: advertiser_tracking_id,
+    });
+
+    if (newUser) {
+      facebookLink = updatedLink;
+      console.log({
+        "New user created with same ip but new advertiserId": newUser,
+      });
       const appStoreLink = process.env.APP_STORE_LINK;
       console.log("app install in progress");
       return res.redirect(appStoreLink);
@@ -168,8 +192,8 @@ async function checkFacebookAppActicationEvent() {
 
 // office
 
-//AASA file path//https://www.plinsters.pro/.well-known/apple-app-site-association
-//associated domain: applinks:www.plinsters.pro
+//AASA file path//https://www.dmtgames.pro/.well-known/apple-app-site-association
+//associated domain: applinks:www.dmtgames.pro
 //Step2: automtically by apple
 // automatic download link for AASA file done by apple from the associated domain list created in xcode only after the app has been installed on the device
 app.get("/.well-known/apple-app-site-association", (req, res) => {
@@ -182,7 +206,7 @@ app.get("/.well-known/apple-app-site-association", (req, res) => {
 
 //step3: on app launch
 // call this on initializing app to fetch back the original link that is needed for tracking user
-// because in the associated domain, we may not have th full path, but only the root domain https://www.plinsters.pro
+// because in the associated domain, we may not have th full path, but only the root domain https://www.dmtgames.pro
 
 app.get("/track_app_installs", async (req, res) => {
   const ip =
@@ -211,7 +235,7 @@ app.get("/track_app_installs", async (req, res) => {
     }
   }
   console.log("checking installs");
-  await createFcaebookAppInstallEvent();
+  await createFacebookAppInstallEvent();
 });
 
 // fetch all users
@@ -245,7 +269,7 @@ app.get("/installed", async (req, res) => {
   }
 });
 
-async function createFcaebookAppInstallEvent() {
+async function createFacebookAppInstallEvent() {
   //Install: fb_mobile_install
 
   const url = `https://graph.facebook.com/${app_id}/activities?access_token=${app_access_token}`;
@@ -294,6 +318,224 @@ async function createFcaebookAppInstallEvent() {
 
 //fbp and token
 //token
+
+//keitaro postback
+//  "https://www.wingsofflimits.pro/create_facebook_purchase_event?fbclid={fbclid}&sub_id_10={_sub_id_10}&external_id={subid}&date={date:U}&client_ip_address={_ip}";
+//  "http://localhost:4000/create_facebook_purchase_event?fbclid={fbclid}&sub_id_10={_sub_id_10}&external_id={subid}&date={date:U}&client_ip_address={_ip}";
+//  "http://localhost:4000/create_facebook_purchase_event?fbclid=user123&sub_id_10=abcdefg&external_id=user123&date={date:U}&client_ip_address={_ip}";
+
+/**
+ * 
+ //keitaro postback without date and client_ip_address
+//  "https://www.wingsofflimits.pro/create_facebook_purchase_event?fbclid={fbclid}&sub_id_10={_sub_id_10}&external_id={subid}&client_ip_address={_ip}";
+//  "http://localhost:4000/create_facebook_purchase_event?fbclid={fbclid}&sub_id_10={_sub_id_10}&external_id={subid}&client_ip_address={_ip}";
+//  "http://localhost:4000/create_facebook_purchase_event?fbclid=user123&sub_id_10=abcdefg&external_id=user123&client_ip_address={_ip}";
+ */
+
+app.get("/create_facebook_purchase_event", async (req, res) => {
+  //Install: fb_mobile_install
+
+  const requestURL = req.originalUrl; // This will include query parameters, if any
+  const { fbclid, sub_id_10, external_id, date, client_ip_address } = req.query;
+
+  const ip =
+    req.headers["cf-connecting-ip"] ||
+    req.headers["x-real-ip"] ||
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress ||
+    "";
+
+  console.log({ userIPAddress: ip });
+  console.log({ requestURL });
+  console.log({ Query: req.query });
+
+  // const sub_id_10 = "";
+  const unixTimeNow = Math.floor(Date.now() / 1000);
+  console.log({ unixTimeNow });
+
+  const min = 1;
+  const max = 9999;
+  let randomNumberFloat = Math.random() * (max - min) + min;
+
+  const random = Math.round(randomNumberFloat);
+  console.log({ random });
+
+  // Function to generate random number
+
+  if (unixTimeNow && random) {
+    console.log({ processing: "calling facebook endpoint" });
+    const url = `https://graph.facebook.com/${app_id}/activities?access_token=${app_access_token}`;
+
+    const payload = {
+      event: "CUSTOM_APP_EVENTS",
+      advertiser_tracking_enabled: 1,
+      application_tracking_enabled: 1,
+      advertiser_id: "1234",
+      action_source: "website",
+      event_time: date || unixTimeNow,
+      event_source_url: "https://av-gameprivacypolicy.site/app",
+      custom_events: [
+        {
+          _eventName: "fb_mobile_purchase",
+          // _eventName: "Purchase", // Standard event name for purchases
+          _valueToSum: 10,
+          fb_currency: "USD",
+        },
+      ],
+      skadnetwork_attribution: {
+        version: "2.2",
+        source_app_id: app_id,
+        // conversion_value: 0,
+        conversion_value: 1,
+      },
+      user_data: {
+        external_id: external_id ? external_id.toString() : "user123",
+        // client_ip_address: client_ip_address || ip,
+        client_ip_address: client_ip_address || "192.168.1.1",
+        client_user_agent:
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        // fbc: "fb.1.1625247600.abcdefg",
+        // fbp: "fb.1.1625247600.1234",
+        fbc: `fb.1.${date || unixTimeNow}.${sub_id_10 ? sub_id_10 : "abcdefg"}`,
+        fbp: `fb.1.${date || unixTimeNow}.${random}`,
+        fbclid: fbclid || null, // Include fbclid if available
+      },
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    try {
+      const response = await axios.post(url, payload, { headers: headers });
+
+      if (response.data) {
+        let result = response.data;
+
+        console.log({ FB_purchase_event_result: result });
+        //{ result: { success: true } }
+      }
+      //====={New update}========================
+    } catch (error) {
+      // const err = error.response.data;
+      console.log({ FB_purchase_event_error: error });
+      // console.error(error);
+      if (error.response) {
+        // Server responded with a status code outside the range of 2xx
+        console.error("Error response data:", error.response.data);
+        console.error("Error response status:", error.response.status);
+        console.error("Error response headers:", error.response.headers);
+      } else if (error.request) {
+        // No response received
+        console.error("Error request data:", error.request);
+      } else {
+        // Something else happened
+        console.error("Error message:", error.message);
+      }
+    }
+  }
+});
+
+app.get("/create_facebook_leads_event", async (req, res) => {
+  //Install: fb_mobile_install
+
+  const requestURL = req.originalUrl; // This will include query parameters, if any
+  const { fbclid, sub_id_10, external_id, date, client_ip_address } = req.query;
+
+  const ip =
+    req.headers["cf-connecting-ip"] ||
+    req.headers["x-real-ip"] ||
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress ||
+    "";
+
+  console.log({ userIPAddress: ip });
+  console.log({ requestURL });
+  console.log({ Query: req.query });
+
+  // const sub_id_10 = "";
+  const unixTimeNow = Math.floor(Date.now() / 1000);
+
+  const min = 1;
+  const max = 9999;
+  let random = Math.random() * (max - min) + min;
+
+  // Function to generate random number
+
+  if (unixTimeNow && random) {
+    const url = `https://graph.facebook.com/${app_id}/activities?access_token=${app_access_token}`;
+
+    const payload = {
+      event: "CUSTOM_APP_EVENTS",
+      advertiser_tracking_enabled: 1,
+      application_tracking_enabled: 1,
+      advertiser_id: "1234",
+      action_source: "website",
+      event_time: date || unixTimeNow,
+      event_source_url: "https://av-gameprivacypolicy.site/app",
+      custom_events: [
+        {
+          // _eventName: "fb_mobile_purchase",
+          _eventName: "Lead", // Standard event name for leads
+          _valueToSum: 0,
+          fb_currency: "USD",
+        },
+      ],
+      skadnetwork_attribution: {
+        version: "2.2",
+        source_app_id: app_id,
+        // conversion_value: 0,
+        conversion_value: 1,
+      },
+      user_data: {
+        external_id: external_id ? external_id.toString() : "user123",
+        client_ip_address: client_ip_address || ip,
+        client_user_agent:
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        // fbc: "fb.1.1625247600.abcdefg",
+        // fbp: "fb.1.1625247600.1234",
+        fbc: `fb.1.${date || unixTimeNow}.${sub_id_10 ? sub_id_10 : "abcdefg"}`,
+        fbp: `fb.1.${date || unixTimeNow}.${random}`,
+        fbclid: fbclid || null, // Include fbclid if available
+      },
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    try {
+      const response = await axios.post(url, payload, { headers: headers });
+
+      if (response.data) {
+        let result = response.data;
+
+        console.log({ FB_purchase_event_result: result });
+        //{ result: { success: true } }
+      }
+      //====={New update}========================
+    } catch (error) {
+      // const err = error.response.data;
+      console.log({ FB_purchase_event_error: error });
+      console.error(error);
+      // return { status: err.success, message: err.message };
+      // res.json(err);
+    }
+  }
+});
+
+/**
+ * Returns the current UNIX timestamp.
+ *
+ * @returns {Number}
+ */
+async function unixTimestamp() {
+  return Math.floor(Date.now() / 1000);
+}
+
+async function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 // Error Middleware
 app.use(errorHandler);
